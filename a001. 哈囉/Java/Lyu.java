81362a80ba19408befe0d41d625bf894a258e5be
Yu-Wei,Lyu <awaia732@gmail.com>
2022-07-20 11:21:43 +0800

    Lyu a001. java

---
+++ a001. 哈囉/Java/Lyu.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Lyu {
+    public static void main(String[] args) {
+        // a001
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello, " + scanner.nextLine());
+    }
+}
